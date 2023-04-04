e0a4de46a2a2a1a3235cf9dd48d312e2682c5313
Imam Taufiq Hermawan <imamtaufiq133@gmail.com>
2023-04-04 09:04:00 +0700

    config to postgresql database with sequelize

---
+++ config/database.js
@@ -0,0 +1,9 @@
+module.exports = {
+    development: {
+        username: "postgres",
+        password: "admin123",
+        database: "database_development",
+        host: "localhost",
+        dialect: "postgres"
+    },
+}
